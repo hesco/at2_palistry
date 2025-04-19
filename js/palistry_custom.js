250904aca8f937e5b30d0cd7387043fb35620eed
Hugh Esco <hesco@yourmessagedelivered.com>
2025-04-19 19:13:51 -0400

    clean up white space from email

--- js/palistry_custom.js
+++ js/palistry_custom.js
@@ -4,19 +4,19 @@ jQuery(document).ready(function () {
 
 (function($){
   var stripeBtnUpdate = function(selector){
-    var $btn1 = $(selector);
-    if($btn1.length > 0){
-      var sr = $btn1[0].shadowRoot;
-      if(sr){
-        $(sr).find('iframe').css('width','100%');
-      }
-    }
-  };
-  $(function(){
-    setTimeout(function(){
-      stripeBtnUpdate('header .donate_now_button stripe-buy-button');
-      stripeBtnUpdate('.palistory_contribute_now_card .stripe_buy_now_button stripe-buy-button');
-    }, 1000);
-  });
+    var $btn1 = $(selector);
+    if($btn1.length > 0){
+      var sr = $btn1[0].shadowRoot;
+      if(sr){
+        $(sr).find('iframe').css('width','100%');
+      }
+    }
+  };
+  $(function(){
+    setTimeout(function(){
+      stripeBtnUpdate('header .donate_now_button stripe-buy-button');
+      stripeBtnUpdate('.palistory_contribute_now_card .stripe_buy_now_button stripe-buy-button');
+    }, 1000);
+  });
 })(jQuery);
 
