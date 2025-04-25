e5279568297313c2fedb7c4fa35b0e4bbaf60fb4
Hugh Esco <hesco@campaignfoundations.com>
2025-04-25 16:00:00 +0000

    enhance sync_theme_dev.sh

--- sync_theme_dev.sh
+++ sync_theme_dev.sh
@@ -4,4 +4,7 @@ cd /var/lib/jenkins/sites/palistry/web/themes/custom/at2_palistry
 branch_name=$(git branch | grep ^* | sed "s,* ,,")
 git pull gh ${branch_name} --tags
 drush cr
+git describe --tags > theme_version.txt
+git branch >> theme_version.txt
+git status >> theme_version.txt
 
